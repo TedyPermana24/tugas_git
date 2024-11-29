6cc0d97ca71ba5b86a64c951965df443c45806e9
TedyPermana24 <tedysukma24@gmail.com>
2024-11-29 22:33:15 +0700

    change variable

--- tugas_git.py
+++ tugas_git.py
@@ -1,4 +1,4 @@
-panen = {
+data_panen = {
     'lokasi1' : {
         'nama_lokasi' : 'Kebun A',
         'hasil_panen' : {
@@ -42,22 +42,22 @@ panen = {
 } 
 
 #1. Tampilkan seluruh data dari data panen termasuk nama lokasi dan hasil panen masing-masing.
-for i,j in panen.items() :
+for i,j in data_panen.items() :
     print(f"Lokasi : {i}")
     print(f"Tempat : {j["nama_lokasi"]}")
     print(f"Hasil Panen : {j["hasil_panen"]}")
 
 #2. Tampilkan jumlah hasil panen jagung dari lokasi2.
-print(panen['lokasi2']["hasil_panen"]["jagung"])
+print(data_panen['lokasi2']["hasil_panen"]["jagung"])
 
 #3. Tampilkan nama lokasi dari lokasi3.
-print(panen['lokasi3']["nama_lokasi"])
+print(data_panen['lokasi3']["nama_lokasi"])
 
 #4. Masukkan Jumlah Hasil Panen Padi dan Kedelai Setiap Lokasi ke Dalam Variabel yang Berbeda:
 #5. Buat variabel terpisah untuk menyimpan jumlah hasil panen padi dan kedelai dari setiap lokasi.
 panen_padi = []
 panen_kedelai = []
-for i in panen.values() :
+for i in data_panen.values() :
    panen_padi.append(i["hasil_panen"]["padi"])
    panen_kedelai.append(i["hasil_panen"]["kedelai"])
 
@@ -65,7 +65,7 @@ print(panen_padi)
 print(panen_kedelai)
 
 #6 Buat Percabangan jika jumlah hasil panen padi lebih dari 1300 atau jagung lebih dari 800 di suatu lokasi, maka lokasi tersebut memerlukan perh
-for i,j in panen.items() :
+for i,j in data_panen.items() :
     if(j['hasil_panen']['padi'] > 1300 or j['hasil_panen']['jagung'] > 800) :
         print(f"{i} yaitu di {j['nama_lokasi']}, memerlukan perhatian khusus")
     else :
